042eac16d356041bab1d02be10e60551e970f375
test <xnicecraftacc@gmail.com>
2023-10-19 11:11:22 +0700

    Add missing getgrnam_r, ifaddrs, __fsetlocking, etc for android 5 and 6

---
+++ src/getgrnam_r.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <grp.h>
+#include <android/api-level.h>
+
+#if __ANDROID_API__ < 24
+
+// Credit: https://github.com/chombourger/android-udev/blob/c78aaf59cdc4378346eb084e9cdc02ca46bc389a/android/missing.c
+int getgrnam_r(const char *name, struct group *grp, char *buf, size_t buflen, struct group **result)
+{
+	struct group *g = getgrnam(name);
+
+	if (g != NULL) {
+		grp->gr_gid = g->gr_gid;
+		strncpy(buf, g->gr_name, buflen);
+		grp->gr_name = buf;
+		grp->gr_passwd = NULL;
+		grp->gr_mem = NULL;
+		*result = grp;
+		return 0;
+	}
+	*result = NULL;
+	return -1;
+}
+#endif
